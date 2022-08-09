fa5b13abe517eb28dd4bbd51c32bce4595895c3a
Rocío Dorado <rocio.dorado.12@hotmail.com>
2022-08-08 23:12:53 -0300

    Login y registro creado-maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,19 @@ import {BrowserRouter} from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import RoutesApp from './Routes/RoutesApp'
+import LoginScreen from './pages/LoginScreen'
+import RegisterScreen from './pages/RegisterScreen'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
-    <RoutesApp/>
+    <RoutesApp>
+      <Route/>
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/registro" element={<RegisterScreen />} />
+    </RoutesApp>
     </BrowserRouter>
   )
 }
