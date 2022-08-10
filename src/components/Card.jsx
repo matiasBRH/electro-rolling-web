a62f9948bc1b6b1f14cedbf85657177126edc60f
Santi Comolli <santi.comolli@gmail.com>
2022-08-10 18:37:29 -0300

    Traigo la rama de amtias

---
+++ src/components/Card.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../css/card.css'
+
+const Card = (props) => {
+    console.log(props)
+    const {producto} = props
+    return (
+        <div className="col-3 col-md-3 col-lg-3">
+        <div className="container d-flex justify-content-center">
+        <figure className="card card-product-grid card-lg"> <a href="#" className="img-wrap" data-abc="true"><img src={producto.imagen}></img></a>
+            <figcaption className="info-wrap">
+                <div className="row">
+                    <div className="col-md-9"> <a href="#" className="title" data-abc="true">{producto.nombre}</a> </div>
+                    <div className="col-md-3">
+                        <div className="rating text-right"> <i className="fa fa-star"></i> <i className="fa fa-star"></i> <i className="fa fa-star"></i> </div>
+                    </div>
+                </div>
+            </figcaption>
+            <div className="bottom-wrap"> <a href="#" className="btn btn-primary float-right" data-abc="true"> Buy now </a>
+                <div className="price-wrap"> <span className="price h5">${producto.precio}</span> <br></br> <small className="text-success">Free shipping</small> </div>
+            </div>
+        </figure>
+        </div>
+        </div>
+    )
+}
+
+export default Card
--- src/pages/SearchScreen.jsx
+++ src/pages/SearchScreen.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Grid from '../components/Grid'
+
 
 const SearchScreen = () => {
   
   return (
 
     <div>
-
+      <Grid/>
     </div>
   )
 }
