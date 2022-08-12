2dd88cf09e0f82ae6b424adc5dfe458fa9c46ed2
Pablo Marino <pmarino2013@gmail.com>
2022-08-12 09:24:01 -0300

    Arreglando rutas, agregando modal cuando presiono boton inicio de sesion

--- src/Routes/RoutesApp.jsx
+++ src/Routes/RoutesApp.jsx
@@ -1,28 +1,31 @@
-import React from 'react'
-import {Routes,Route} from 'react-router-dom'
-import AboutScreen from '../pages/AboutScreen'
-import AdminScreen from '../pages/AdminScreen'
-import CartScreen from '../pages/CartScreen'
-import ErrorScreen from '../pages/ErrorScreen'
-import FavoritosScreen from '../pages/FavoritosScreen'
-import HomeScreen from '../pages/HomeScreen'
-import RegisterScreen from '../pages/RegisterScreen'
-import SearchScreen from '../pages/SearchScreen'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import NavbarApp from "../components/NavbarApp";
+import AboutScreen from "../pages/AboutScreen";
+import AdminScreen from "../pages/AdminScreen";
+import CartScreen from "../pages/CartScreen";
+import ErrorScreen from "../pages/ErrorScreen";
+import FavoritosScreen from "../pages/FavoritosScreen";
+import HomeScreen from "../pages/HomeScreen";
+import RegisterScreen from "../pages/RegisterScreen";
+import SearchScreen from "../pages/SearchScreen";
 
 const RoutesApp = () => {
-
   return (
-    <Routes >
-        <Route path="/" element={<HomeScreen/>}/>
-        <Route path="/admin" element={<AdminScreen/>}/>
-        <Route path="/about" element={<AboutScreen/>}/>
-        <Route path="/register" element={<RegisterScreen/>}/>
-        <Route path="/search" element={<SearchScreen/>}/>
-        <Route path="/cart" element={<CartScreen/>}/>
-        <Route path="/favorites" element={<FavoritosScreen/>}/>
-        <Route path="*" element={<ErrorScreen/>}/>
-    </Routes>
-  )
-}
+    <>
+      <NavbarApp />
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/admin" element={<AdminScreen />} />
+        <Route path="/about" element={<AboutScreen />} />
+        <Route path="/register" element={<RegisterScreen />} />
+        <Route path="/search" element={<SearchScreen />} />
+        <Route path="/cart" element={<CartScreen />} />
+        <Route path="/favorites" element={<FavoritosScreen />} />
+        <Route path="*" element={<ErrorScreen />} />
+      </Routes>
+    </>
+  );
+};
 
-export default RoutesApp
+export default RoutesApp;
