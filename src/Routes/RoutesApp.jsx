4584d52242e5e92d8ca357be05c0cb5ea6405f91
Santi Comolli <santi.comolli@gmail.com>
2022-08-14 21:03:33 -0300

    ..

--- src/Routes/RoutesApp.jsx
+++ src/Routes/RoutesApp.jsx
@@ -3,15 +3,11 @@ import {Routes,Route} from 'react-router-dom'
 import HomeScreen from '../pages/HomeScreen'
 import AdminScreen from '../pages/AdminScreen'
 import SearchScreen from '../pages/SearchScreen'
-<<<<<<< HEAD
-import LoginScreen from '../pages/LoginScreen'
-=======
 import CartScreen from '../pages/CartScreen'
 import AboutScreen from '../pages/AboutScreen'
 import ContactoScreen from '../pages/ContactoScreen'
 import FavoritosScreen from '../pages/FavoritosScreen'
 import ErrorScreen from '../pages/ErrorScreen'
->>>>>>> a856fa6950ee2b6906bd89d764e92f20577abe8c
 
 const RoutesApp = () => {
 
@@ -23,12 +19,8 @@ const RoutesApp = () => {
         <Route path="/about" element={<AboutScreen/>}/>
         <Route path="/contacto" element={<ContactoScreen/>}/>
         <Route path="/favorites" element={<FavoritosScreen/>}/>
-<<<<<<< HEAD
-        <Route path="/login" element={<LoginScreen/>}/>
-=======
         <Route path="/admin" element={<AdminScreen/>}/>
         <Route path="/cart" element={<CartScreen/>}/>
->>>>>>> a856fa6950ee2b6906bd89d764e92f20577abe8c
         <Route path="*" element={<ErrorScreen/>}/>
     </Routes>
   )
