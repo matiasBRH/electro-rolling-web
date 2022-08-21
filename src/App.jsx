b4f35b0f695a6949c6590e3114524969043c589a
mat <matbarahona71@gmail.com>
2022-08-20 21:36:48 -0300

    arreglando rutas

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import RoutesApp from "./Routes/RoutesApp";
+import LoginScreen from "./pages/LoginScreen";
+import RegistroScreen from "./pages/RegistroScreen";
 
 function App() {
   return (
-    
-
     <BrowserRouter>
       <Routes>
-      <NavbarApp/>
-      <RoutesApp/>
-      <Footer/>
+        <Route path="/*" element={<RoutesApp />} />
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/registro" element={<RegistroScreen />} />
       </Routes>
     </BrowserRouter>
   );
--- src/Routes/RoutesApp.jsx
+++ src/Routes/RoutesApp.jsx
@@ -1,30 +1,34 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import NavbarApp from "../components/NavbarApp";
+
 import AboutScreen from "../pages/AboutScreen";
 import ContactoScreen from '../pages/ContactoScreen'
 import FavoritosScreen from '../pages/FavoritosScreen'
 import RegistroScreen from '../pages/RegistroScreen'
+import SearchScreen from "../pages/SearchScreen";
 import PasswordScreen from '../pages/PasswordScreen'
 import SendScreen from '../pages/SendScreen'
-import ErrorScreen from '../pages/ErrorScren';
+import ErrorScreen from '../pages/ErrorScreen';
+import HomeScreen from "../pages/HomeScreen";
+import Footer from "../components/Footer";
 
 const RoutesApp = () => {
   return (
      <>
       <NavbarApp />
       <Routes>
-        <Route path="/" element={<HomeScreen />} />
+        <Route path="/" element={<HomeScreen />} />        
         <Route path="/about" element={<AboutScreen />} />
         <Route path="/contacto" element={<ContactoScreen/>}/>
-        <Route path="/favorites" element={<FavoritosScreen/>}/>
-        <Route path="/registro" element={<RegistroScreen/>}/>
+                <Route path="/favorites" element={<FavoritosScreen/>}/>
+        <Route path="/register" element={<RegistroScreen/>}/>
         <Route path="/password" element={<PasswordScreen/>}/>
-        <Route path="/send" element={<SendScreen/>}/>
-       
-        <Route path="*" element={<ErrorScreen/>}/>
-        
-    </Routes>
+        <Route path="/send" element={<SendScreen/>}/>  
+        <Route path="/search" element={<SearchScreen />} />             
+        <Route path="*" element={<ErrorScreen/>}/>        
+        </Routes>
+    <Footer/>
     </>
   )
 }
