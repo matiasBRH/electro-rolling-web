6ab1e333ac65a3eb6d59dddabba7246ebae76591
mat <matbarahona71@gmail.com>
2022-08-28 21:28:06 -0300

    cards destacadas home

--- src/components/CardDestacadas.jsx
+++ src/components/CardDestacadas.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from "react-router-dom";
 import "../css/card.css"
 import '../css/destacados.css'
 
@@ -12,10 +12,10 @@ const CardDestacadas = (props) => {
     return (
             
         <div className="card m-1 ms-1 destacadas">
-            <img src={producto.imagen} className="card-img-top" alt="..."/>
+            <img src={producto.img} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h6 className="card-title">{producto.nombre}</h6>
-                <a href="#" className="btn btn-success">Ver producto</a>
+                <Link to={`/productos/${producto._id}`} className="btn btn-card-product btn-primary float-right img-wrap" data-abc="true">Ver producto</Link>
             </div>
         </div>
 
